c78c6c23fe29e5853655df1cbdc6f22c0958d6a9
Meesum A. Qazalbash <mesumali26.ma@gmail.com>
2022-06-03 02:21:53 +0500

    docs left

--- wordle.cpp
+++ wordle.cpp
@@ -1,22 +1,11 @@
 #include "wordle.hpp"
 
-Wordle::Wordle(std::string fn) : filename(fn)
+Wordle::Wordle(std::string fn)
 {
-    fill_bucket();
+    fill_bucket(fn);
 }
 
-template <class T>
-void print(std::string message, std::set<T> set)
-{
-    std::cout << message;
-    for (auto set_iter = set.begin(); set_iter != set.end(); ++set_iter)
-    {
-        std::cout << *set_iter << "\t";
-    }
-    std::cout << std::endl;
-}
-
-void Wordle::fill_bucket()
+void Wordle::fill_bucket(std::string filename)
 {
     file.open(filename);
     std::string w;
@@ -26,172 +15,6 @@ void Wordle::fill_bucket()
         bucket.insert(w);
     }
     default_bucket = bucket;
-}
-
-void Wordle::guess_word(std::string word)
-{
-    bool keep;
-    std::set<std::string> demo;
-    std::string matched_word;
-
-    demo = bucket;
-
-    for (std::set<std::string>::iterator bucket_item = bucket.begin();
-         bucket_item != bucket.end(); ++bucket_item)
-    /*
-    removing all the elements that contain any of the black letter
-    or does not contain any yellow letter
-    */
-    {
-        matched_word = *bucket_item;
-        keep = true;
-        for (std::set<char>::iterator black_it = not_allowed.begin();
-             black_it != not_allowed.end(); ++black_it)
-        {
-            if (matched_word.find(*black_it) != std::string::npos)
-            {
-                demo.erase(matched_word);
-                break;
-            }
-        }
-
-        for (std::set<char>::iterator yellow_it = allowed.begin();
-             yellow_it != allowed.end(); ++yellow_it)
-        {
-            if (matched_word.find(*yellow_it) == std::string::npos)
-            {
-                demo.erase(matched_word);
-                break;
-            }
-        }
-    }
-
-    bucket = demo;
-
-    for (std::set<std::string>::iterator match_it = bucket.begin();
-         match_it != bucket.end(); ++match_it)
-    // passing only those elements which have same green elements
-    {
-        keep = true;
-        matched_word = *match_it;
-        for (int i = 0; i < 5; i++)
-        {
-            if (final_word[i] != '*' && matched_word[i] != final_word[i])
-            {
-                demo.erase(matched_word);
-                break;
-            }
-        }
-    }
-
-    bucket = demo;
-
-    print("possible words = ", bucket);
-
-    if (bucket.size() == 1)
-    {
-        final_word = *bucket.begin();
-    }
-}
-
-template <class T>
-void Wordle::input(std::string message, T &var)
-{
-    std::cout << std::endl
-              << message;
-    std::cin >> var;
-    std::cout << std::endl;
-}
-
-inline bool Wordle::warning(std::string message, std::string var, const int size)
-{
-    if (var.size() != size)
-    {
-        std::cout << message << " " << size << std::endl;
-        return true;
-    }
-    return false;
-}
-
-void Wordle::start()
-{
-    std::string input_word, result;
-    std::cout << "-------------------------------------------------------"
-              << "-------------------------------------------------------"
-              << "-------------------------------------------------------"
-              << "-------------------------------------------------------" << std::endl;
-
-    do
-    {
-        input("Enter the word: ", input_word);
-    } while (warning("Word should be of length", input_word, 5));
-
-    if (default_bucket.find(input_word) == default_bucket.end())
-    {
-        std::cout << "Word is not in the list" << std::endl;
-    }
-
-    do
-    {
-        input("Enter the result: ", result);
-    } while (warning("Word should be of length", result, 5));
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (result[i] == 'G' || result[i] == 'g')
-        {
-            if (final_word[i] == '*')
-            {
-                final_word[i] = input_word[i];
-                // yellow.erase(input_word[i]);
-            }
-        }
-        else if (result[i] == 'Y' || result[i] == 'y')
-        {
-            allowed.insert(input_word[i]);
-        }
-        else if (result[i] == 'B' || result[i] == 'b' ||
-                 result[i] == 'W' || result[i] == 'w')
-        {
-            if (allowed.find(input_word[i]) == allowed.end())
-            {
-                not_allowed.insert(input_word[i]);
-            }
-        }
-        else
-        {
-            std::cout << "The input is wrong" << std::endl;
-            start();
-        }
-    }
-
-    std::cout << std::endl
-              << std::endl
-              << "Word is becoming = " << final_word << std::endl
-              << std::endl
-              << std::endl;
-
-    // for (std::set<char>::iterator it = allowed.begin();
-    //      it != allowed.end(); ++it)
-    // {
-    //     if (not_allowed.find(*it) != not_allowed.end())
-    //     {
-    //         not_allowed.erase(*it);
-    //     }
-    // }
-
-    // for (std::string::iterator i = final_word.begin(); i != final_word.end(); ++i)
-    // {
-    //     if (*i != '*' && not_allowed.find(*i) != not_allowed.end())
-    //     {
-    //         not_allowed.erase(*i);
-    //     }
-    // }
-
-    // print("Letters     in the word = ", allowed);
-    // print("Letters not in the word = ", not_allowed);
-
-    guess_word(input_word);
 }
 
 void Wordle::game()
@@ -212,4 +35,173 @@ void Wordle::game()
             start();
         }
     }
+}
+
+template <class T>
+void input(std::string message, T &var)
+{
+    std::cout << std::endl
+              << message;
+    std::cin >> var;
+    std::cout << std::endl;
+}
+
+template <class T>
+void print(std::string message, std::set<T> set)
+{
+    std::cout << message;
+    for (auto set_iter = set.begin(); set_iter != set.end(); ++set_iter)
+    {
+        std::cout << *set_iter << "\t";
+    }
+    std::cout << std::endl;
+}
+
+void Wordle::start()
+{
+    std::string input_word, result;
+    std::cout << "------------------------------------"
+              << "------------------------------------"
+              << "------------------------------------"
+              << "------------------------------------" << std::endl;
+
+    do
+    {
+        input("Enter the word: ", input_word);
+    } while (warning("Word should be of length", input_word, 5) ||
+             default_bucket.find(input_word) == default_bucket.end());
+
+    do
+    {
+        input("Enter the result: ", result);
+    } while (warning("Word should be of length", result, 5));
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (result[i] == 'G' || result[i] == 'g')
+        {
+            if (final_word[i] == '*')
+            {
+                final_word[i] = input_word[i];
+
+                if (yellow.find(input_word[i]) != yellow.end())
+                {
+                    yellow.erase(input_word[i]);
+                }
+
+                for (int j = 97; j < 123; j++)
+                {
+                    if (j != (int)input_word[i])
+                    {
+                        no_map[i].insert((char)j);
+                    }
+                }
+            }
+        }
+
+        else if (result[i] == 'Y' || result[i] == 'y')
+        {
+            no_map[i].insert(input_word[i]);
+            yellow.insert(input_word[i]);
+        }
+
+        else if (result[i] == 'B' || result[i] == 'b' ||
+                 result[i] == 'W' || result[i] == 'w')
+        {
+            if (yellow.find(input_word[i]) == yellow.end() &&
+                final_word.find(input_word[i]) == std::string::npos)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    no_map[j].insert(input_word[i]);
+                }
+            }
+        }
+
+        else
+        {
+            std::cout << "The input is wrong" << std::endl;
+            start();
+        }
+    }
+
+    std::cout << std::endl
+              << std::endl
+              << "Word is becoming = " << final_word << std::endl
+              << std::endl
+              << std::endl;
+
+    for (std::map<int, std::set<char>>::iterator it = no_map.begin();
+         it != no_map.end(); ++it)
+    {
+        std::cout << (*it).first << ": ";
+        for (std::set<char>::iterator it2 = ((*it).second).begin();
+             it2 != ((*it).second).end(); it2++)
+        {
+            std::cout << *it2 << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    guess_word(input_word);
+}
+
+void Wordle::guess_word(std::string word)
+{
+    int index;
+    std::set<std::string> demo = bucket;
+
+    for (std::map<int, std::set<char>>::iterator it = no_map.begin();
+         it != no_map.end(); ++it)
+    {
+        index = (*it).first;
+
+        for (std::set<char>::iterator it2 = (*it).second.begin();
+             it2 != (*it).second.end(); ++it2)
+        {
+            for (std::set<std::string>::iterator it3 = bucket.begin();
+                 it3 != bucket.end(); ++it3)
+            {
+                if ((*it3)[index] == *it2)
+                {
+                    demo.erase(*it3);
+                }
+            }
+        }
+    }
+
+    bucket = demo;
+
+    for (std::set<std::string>::iterator it = bucket.begin();
+         it != bucket.end(); ++it)
+    {
+        for (std::set<char>::iterator it2 = yellow.begin();
+             it2 != yellow.end(); ++it2)
+        {
+            if ((*it).find(*it2) == std::string::npos)
+            {
+                demo.erase(*it);
+            }
+        }
+    }
+
+    bucket = demo;
+
+    print("possible words = ", bucket);
+    // print("Yellows: ", yellow);
+
+    if (bucket.size() == 1)
+    {
+        final_word = *bucket.begin();
+    }
+}
+
+bool Wordle::warning(std::string message, std::string var, const int size)
+{
+    if (var.size() != size)
+    {
+        std::cout << message << " " << size << std::endl;
+        return true;
+    }
+    return false;
 }
--- wordle.hpp
+++ wordle.hpp
@@ -13,17 +13,14 @@ public:
 
 private:
     std::ifstream file;
-    std::set<char> allowed, not_allowed;
-    std::map<int, std::set<int>> no_map;
-    std::string filename, final_word = "*****";
+    std::set<char> yellow;
+    std::map<int, std::set<char>> no_map;
+    std::string final_word = "*****";
     std::set<std::string> bucket = {}, default_bucket = {};
 
     void start();
-    void fill_bucket();
     void guess_word(std::string);
+    void fill_bucket(std::string);
 
-    template <class T>
-    void input(std::string, T &);
-
-    inline bool warning(std::string, std::string, const int);
+    bool warning(std::string, std::string, const int);
 };
